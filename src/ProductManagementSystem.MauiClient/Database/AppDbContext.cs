f0bc616e6662f60ab7ca1f4f501f31a519afc194
İbrahim Sarıgöz <srgzibrahim@gmail.com>
2024-09-27 20:35:10 +0300

    ProductItem lar için appdbcontext oluşturuldu

---
+++ src/ProductManagementSystem.MauiClient/Database/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem.MauiClient.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.MauiClient.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ProductItem> Products { get; set; }
+    }
+}
