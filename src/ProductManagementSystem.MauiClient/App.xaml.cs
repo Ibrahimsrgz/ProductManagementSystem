a771d755c2038aa05ec02fe38f8696fe06326a69
İbrahim Sarıgöz <srgzibrahim@gmail.com>
2024-09-29 12:01:30 +0300

    fixed.

--- src/ProductManagementSystem.MauiClient/App.xaml.cs
+++ src/ProductManagementSystem.MauiClient/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace ProductManagementSystem.MauiClient;
+﻿using ProductManagementSystem.MauiClient.Database;
+
+namespace ProductManagementSystem.MauiClient;
 
 public partial class App : Application
 {
-	public App()
+	public App(MainPage mainPage)
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+		//MainPage = new AppShell();
+        MainPage = new NavigationPage(mainPage); // Ensure MainPage is resolved through DI
+
+    }
 }
