05aa32192947861331962c43df624bd4de378f60
İbrahim Sarıgöz <srgzibrahim@gmail.com>
2024-09-29 11:48:42 +0300

    controlden gecti

--- src/ProductManagementSystem.MauiClient/MauiProgram.cs
+++ src/ProductManagementSystem.MauiClient/MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ProductManagementSystem.MauiClient.Database;
+using ProductManagementSystem.MauiClient.Pages;
+using System.Reflection;
 
 namespace ProductManagementSystem.MauiClient;
 
@@ -6,21 +11,45 @@ public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
+        var configuration =Helpers.ConfigurationHelper.LoadConfiguration();
 
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-        //builder.Services.AddHttpClient();
+        // Veritabanı dosya yolunu yapılandırmadan alın
+        string dbName = configuration.GetSection("DatabaseSettings:DatabaseName").Value;
 
-        return builder.Build();
-	}
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, dbName);
+
+        // Register AppDbContext for DI
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite($"Filename={dbPath}");
+        }, ServiceLifetime.Transient);
+
+        // Register ProductListPage for DI
+        builder.Services.AddTransient<ProductListPage>();
+
+        // Detail page her defasında yeni bir instance oluşturur
+        builder.Services.AddTransient<ProductDetailPage>();  
+
+        // Register MainPage for DI and pass IServiceProvider
+        builder.Services.AddSingleton<MainPage>();
+
+        var app = builder.Build();
+
+        // Veritabanını oluştur
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            dbContext.Database.EnsureCreated();
+        }
+
+        return app;
+    }
 }
