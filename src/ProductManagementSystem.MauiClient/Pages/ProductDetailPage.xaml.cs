16a4987099b7ea1f2c2b9cbb2056ff520f37d986
İbrahim Sarıgöz <srgzibrahim@gmail.com>
2024-09-29 11:52:31 +0300

    ProductDetailPage eklendi

---
+++ src/ProductManagementSystem.MauiClient/Pages/ProductDetailPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ProductManagementSystem.MauiClient.Dtos.Product;
+using ProductManagementSystem.MauiClient.ViewModels;
+
+namespace ProductManagementSystem.MauiClient.Pages
+{
+    public partial class ProductDetailPage : ContentPage
+    {
+        public ProductDetailPage(ProductItem selectedItem)
+        {
+            InitializeComponent();
+            BindingContext = selectedItem; // Veriyi sayfaya bağla
+        }
+    }
+}
